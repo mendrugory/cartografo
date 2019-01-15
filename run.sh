c68c8f2501ad43bf5a070c48b1c165cbf01f926d
Gonzalo Gabriel Jiménez Fuentes <iam@mendrugory.com>
2019-01-15 11:10:46 +0100

    First commit

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-venv/bin/python main.py
--- venv_creation.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf venv
-
-virtualenv --no-site-packages  -p /usr/bin/python3 venv
-venv/bin/pip3 install --upgrade pip
-venv/bin/pip3 install --no-cache-dir -r requirements.txt --process-dependency-links
